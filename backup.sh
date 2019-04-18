e4dc42a6d4f685f8e34149f517e0f89b86c57a12
Scott O'Neil <americanwookie@gmail.com>
2019-04-18 13:08:40 +0100

    Initial commit

---
+++ backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install:
+#   sudo apt install s3cmd
+
+set -e
+STAGEDIR=`mktemp -d /tmp/mkbackups.XXXX`
+USED=`df -P /tmp  |grep ^/ | perl -pe 's/.*?(\d+)%.*$/$1/g'`
+if [ $USED -ge 90 ]; then
+  exit 1
+fi
+export TZ=/usr/share/zoneinfo/US/Central
+
+tar -czf $STAGEDIR/smokeping-data.tar.gz ~/binddirs/smokeping/data 2>/dev/null
+/usr/bin/s3cmd -q put --storage-class=STANDARD_IA $STAGEDIR/smokeping-data.tar.gz s3://family-backups/pi/smokeping-data.tar.gz
+rm -rf $STAGEDIR
